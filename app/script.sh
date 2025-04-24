e3a882bafe05566ffb1ed60ba610471d8ce16938
hakim <hlakrout@norsys.fr>
2025-04-24 13:33:55 +0200

    fifth

--- app/script.sh
+++ app/script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 docker build -t tremplin-image .
 container-structure-test test --image tremplin-image:latest --config tests/cst-tests.yaml
+
+# bats tests
+export BATS_LIB_PATH=~/tests/test/test_helper
+bats tests/test.bats.sh
---
+++ app/tests/test.bats.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+setup() {
+  bats_load_library bats-assert
+  docker run --name python_app -p 5000:5000 -d --rm tremplin-image:latest
+  sleep 10
+}
+
+teardown(){
+  echo "tests Ended"
+  docker kill python_app
+}
+
+@test "Verify that the app is lunched and the API respond with correct response." {
+  run curl -o /dev/null -s -w "%{http_code}" http://localhost:5000/me.html
+  assert_success
+  assert_output "200"
+  
+} 
